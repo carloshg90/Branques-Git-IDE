23d4f188a6393ad4c120ad75438235bc2393acdd
carloshg90 <carloshg90@github.com>
2019-10-14 18:34:34 +0200

    Primer commit

---
+++ GitIdeBranques/src/GitIDEBranques.java
@@ -0,0 +1,25 @@
+
+public class GitIDEBranques {
+
+	public static void main(String[] args) {
+		
+		int [] numeros = new int [500000];
+		
+		 int numeroAbuscar = (int) Math.floor(Math.random()*1000+1);
+		 int contador = 0;
+		
+		for (int i = 0; i< numeros.length; i++) {
+			numeros[i] = (int) Math.floor(Math.random()*5000+1);
+		}
+		
+		for (int i = 0; i< numeros.length; i++) {
+			
+			if(numeroAbuscar == numeros[i]) {
+				contador++;
+			}
+		}
+		System.out.println("S'ha trobat el numero "+ numeroAbuscar + " "+contador+" vegades.");
+		
+	}
+
+}
